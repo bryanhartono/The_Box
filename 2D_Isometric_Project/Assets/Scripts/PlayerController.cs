09b62c5b85b1538a7c494d0a68b6372a3a3c8de9
Bryan Hartono <bryanhartono03@gmail.com>
2024-12-10 23:30:58 +0800

    Update player movement
    Updated the player movement and only allow player to move in 4 directions (NE, NW, SE, and SW)

--- 2D_Isometric_Project/Assets/Scripts/PlayerController.cs
+++ 2D_Isometric_Project/Assets/Scripts/PlayerController.cs
@@ -2,19 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Flags]
+public enum MovementKeys : byte
+{
+    None = 0,
+    W = 1 << 0, // 0001
+    A = 1 << 1, // 0010
+    S = 1 << 2, // 0100
+    D = 1 << 3  // 1000
+}
+
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D body;
     public SpriteRenderer spriteRenderer;
     public Animator animator;
 
-    public float walkSpeed;
-    
+    public float moveSpeed = 1.0f;
+
     Vector2 currentDirection;
     Vector2 lastMoveDirection;
-    float idleTime;
 
-    // Update is called once per frame
+    MovementKeys currentKeyPressed = MovementKeys.None;
+
     void Update()
     {
         ProcessInputs();
@@ -22,31 +32,103 @@ public class PlayerController : MonoBehaviour
         AnimateSprite();
     }
 
-    void FixedUpdate() 
+    void FixedUpdate()
     {
-        body.velocity = currentDirection * walkSpeed;    
+        body.velocity = currentDirection * moveSpeed;
     }
 
     void ProcessInputs()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical") * 0.5f;
+        // Initialize movement vector
+        currentDirection = Vector2.zero;
 
-        if ((moveX == 0 && moveY == 0) && (currentDirection.x != 0 || currentDirection.y != 0))
+        // Handle key releases to reset the lock
+        HandleKeyReleases();
+
+        // Assign the first key pressed
+        AssignKeyPressed();
+
+        // Perform movement based on the locked key
+        PerformMovement();
+
+        // Store the last move direction when moving
+        if (currentDirection != Vector2.zero)
         {
             lastMoveDirection = currentDirection;
         }
-
-        currentDirection = new Vector2(moveX, moveY).normalized;
     }
+
+    void HandleKeyReleases()
+    {
+        if (Input.GetKeyUp(KeyCode.W) && (currentKeyPressed & MovementKeys.W) != 0)
+        {
+            currentKeyPressed = MovementKeys.None;
+        }
+        else if (Input.GetKeyUp(KeyCode.S) && (currentKeyPressed & MovementKeys.S) != 0)
+        {
+            currentKeyPressed = MovementKeys.None;
+        }
+        else if (Input.GetKeyUp(KeyCode.A) && (currentKeyPressed & MovementKeys.A) != 0)
+        {
+            currentKeyPressed = MovementKeys.None;
+        }
+        else if (Input.GetKeyUp(KeyCode.D) && (currentKeyPressed & MovementKeys.D) != 0)
+        {
+            currentKeyPressed = MovementKeys.None;
+        }
+    }
+
+    void AssignKeyPressed()
+    {
+        if (currentKeyPressed == MovementKeys.None)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                currentKeyPressed |= MovementKeys.W; // Lock to W
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                currentKeyPressed |= MovementKeys.S; // Lock to S
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                currentKeyPressed |= MovementKeys.A; // Lock to A
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                currentKeyPressed |= MovementKeys.D; // Lock to D
+            }
+        }
+    }
+
+    void PerformMovement()
+    {
+        if ((currentKeyPressed & MovementKeys.W) != 0 && Input.GetKey(KeyCode.W))
+        {
+            currentDirection = new Vector2(0.1f, 0.05f); // Move Northeast
+        }
+        else if ((currentKeyPressed & MovementKeys.S) != 0 && Input.GetKey(KeyCode.S))
+        {
+            currentDirection = new Vector2(-0.1f, -0.05f); // Move Southwest
+        }
+        else if ((currentKeyPressed & MovementKeys.A) != 0 && Input.GetKey(KeyCode.A))
+        {
+            currentDirection = new Vector2(-0.1f, 0.05f); // Move Northwest
+        }
+        else if ((currentKeyPressed & MovementKeys.D) != 0 && Input.GetKey(KeyCode.D))
+        {
+            currentDirection = new Vector2(0.1f, -0.05f); // Move Southeast
+        }
+    }
+
 
     void HandleSpriteFlipX()
     {
-        if (!spriteRenderer.flipX && currentDirection.x < 0)
+        if (currentDirection.x < 0)
         {
             spriteRenderer.flipX = true;
         }
-        else if (spriteRenderer.flipX && currentDirection.x > 0)
+        else if (currentDirection.x > 0)
         {
             spriteRenderer.flipX = false;
         }
